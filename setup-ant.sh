30e0978cb77610064bea4b06c9ffcd5e9038b061
Abel Luck <abel@guardianproject.info>
2013-11-11 17:53:32 +0100

    make setup-ant copy the support library when necessary

--- setup-ant.sh
+++ setup-ant.sh
@@ -10,4 +10,13 @@ projectdir=`dirname $f`
     echo "Updating ant setup in $projectdir:"
     android update lib-project -p $projectdir
 done
+
+if [ -f app/libs/android-support-v4.jar ]; then
+    for f in `find external/ -name android-support-v4.jar`; do
+    libsdir=`dirname $f`
+        echo "Updating support library in $libsdir:"
+        cp -f app/libs/android-support-v4.jar $libsdir
+    done
+fi
+
 android update project -p app/ --subprojects --name "$projectname"
