d572d2095e9ad70bfc05de807acff91a3e92fcc1
uberspot <onexemailx@gmail.com>
2014-02-25 22:17:19 +0200

    move some variables to NConstants

--- app/src/info/guardianproject/notepadbot/NConstants.java
+++ app/src/info/guardianproject/notepadbot/NConstants.java
@@ -2,4 +2,7 @@ package info.guardianproject.notepadbot;
 
 public class NConstants {
 	public static final String SHARED_PREFS_NOTELINES = "use_lines_in_notes";
+	
+	 public static final String TAG = "NoteCipher";
+	 public static final int MAX_STREAM_SIZE = 1000000;
 }
