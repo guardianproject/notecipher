9d41cb9d312bf454ed031116578eda1847eea2b9
Hans-Christoph Steiner <hans@eds.org>
2014-03-10 15:10:45 -0400

    remove any duplicate versions of sqlcipher.jar (i.e. in cacheword)

--- setup-ant.sh
+++ setup-ant.sh
@@ -8,7 +8,7 @@ echo ""
 for f in `find external/ -name project.properties`; do
 projectdir=`dirname $f`
     echo "Updating ant setup in $projectdir:"
-    android update lib-project -p $projectdir
+    android update lib-project --path $projectdir
 done
 
 if [ -f app/libs/android-support-v4.jar ]; then
@@ -19,4 +19,12 @@ if [ -f app/libs/android-support-v4.jar ]; then
     done
 fi
 
-android update project -p app/ --subprojects --name "$projectname"
+if [ -f app/libs/sqlcipher.jar ]; then
+    for f in `find external/ -name sqlcipher.jar`; do
+    libsdir=`dirname $f`
+        echo "Updating support library in $libsdir:"
+        cp -f app/libs/sqlcipher.jar $libsdir
+    done
+fi
+
+android update project --path app/ --subprojects --name "$projectname"
