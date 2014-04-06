9443ce523182dcabe290740af52a3c6674826719
Abel Luck <abel@guardianproject.info>
2014-04-06 12:47:37 +0200

    update setup-ant.sh

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' app/res/values/strings.xml`
+target="android-19"
 
 echo "Setting up build for $projectname"
 echo ""
@@ -8,23 +9,6 @@ echo ""
 for f in `find external/ -name project.properties`; do
 projectdir=`dirname $f`
     echo "Updating ant setup in $projectdir:"
-    android update lib-project --path $projectdir
+    android update lib-project -p $projectdir -t $target
 done
-
-if [ -f app/libs/android-support-v4.jar ]; then
-    for f in `find external/ -name android-support-v4.jar`; do
-    libsdir=`dirname $f`
-        echo "Updating support library in $libsdir:"
-        cp -f app/libs/android-support-v4.jar $libsdir
-    done
-fi
-
-if [ -f app/libs/sqlcipher.jar ]; then
-    for f in `find external/ -name sqlcipher.jar`; do
-    libsdir=`dirname $f`
-        echo "Updating support library in $libsdir:"
-        cp -f app/libs/sqlcipher.jar $libsdir
-    done
-fi
-
-android update project --path app/ --subprojects --name "$projectname"
+android update project -p app/ --subprojects --name "$projectname" -t $target
